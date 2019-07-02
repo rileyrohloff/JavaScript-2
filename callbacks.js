5b9535bde8342d5bebabf3548f37176f1f1877ef
rileyrohloff <rileyrohloff@workfront.com>
2019-07-02 14:48:40 -0600

    worked on callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -2,9 +2,9 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
- 
-  //Given this problem: 
-  
+
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -13,7 +13,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
@@ -37,17 +37,36 @@ last(items, function(last) {
 });
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  result = x + y
+  cb(result)
+};
+sumNums(3, 4, function(sumNums) {
+  console.log(sumNums)
+});
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  result = x * y
+  cb(result)
+};
+
+multiplyNums(10, 2, function(multiplyNums) {
+  console.log(multiplyNums)
+})
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+  cb(list.forEach(function(element) {
+    if(item === element) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }))
+};
+
+contains('Gum', items, function(contains) {
+  console.log(contains);
+});
 
 /* STRETCH PROBLEM */
 
